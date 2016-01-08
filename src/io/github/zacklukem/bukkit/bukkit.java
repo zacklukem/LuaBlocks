4c83cb0c0455258b33758dbf7d992812ff60dfc9
Zachary Mayhew <mayhew.zachary2003@gmail.com>
2016-01-07 16:34:17 -0800

    Added block place and other functions

--- src/io/github/zacklukem/bukkit/bukkit.java
+++ src/io/github/zacklukem/bukkit/bukkit.java
@@ -1,32 +1,74 @@
 package io.github.zacklukem.bukkit;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.luaj.vm2.Lua;
 import org.luaj.vm2.LuaValue;
-import org.luaj.vm2.lib.OneArgFunction;
-import org.luaj.vm2.lib.TwoArgFunction;
+import org.luaj.vm2.Varargs;
+import org.luaj.vm2.lib.*;
 
 /**
  * Created by zmayhew on 1/6/16.
  */
 public class bukkit extends TwoArgFunction {
+
+    public static final Varargs EMPTY = new Varargs() {
+        @Override
+        public LuaValue arg(int i) {
+            return null;
+        }
+
+        @Override
+        public int narg() {
+            return 0;
+        }
+
+        @Override
+        public LuaValue arg1() {
+            return null;
+        }
+
+        @Override
+        public Varargs subargs(int i) {
+            return null;
+        }
+    };
+
     public bukkit() {}
 
     public LuaValue call(LuaValue modname, LuaValue env) {
         LuaValue library = tableOf();
-        LuaValue logger = tableOf();
 
         logger: {
+            LuaValue logger = tableOf();
             logger.set("info", new loggerInfo());
             logger.set("warning", new loggerWarning());
             logger.set("loggersevere", new loggerSevere());
             library.set("logger", logger);
         }
 
-
+        block: {
+            LuaValue block = tableOf();
+            block.set("placeBlock", new blockPlaceBlock());
+            library.set("block", block);
+        }
 
         env.set( "bukkit", library );
         return library;
 
+    }
+
+    static class blockPlaceBlock extends LibFunction {
+        @Override
+        public Varargs invoke(Varargs varargs) {
+            int x = varargs.arg1().checkint();
+            int y = varargs.arg(2).checkint();
+            int z = varargs.arg(3).checkint();
+            String block = varargs.arg(4).checkjstring();
+            Bukkit.getServer().getWorld("world").getBlockAt(x,y,z).setType(Material.getMaterial(block));
+            return EMPTY;
+        }
     }
 
     static class loggerInfo extends OneArgFunction {
--- src/io/github/zacklukem/luablocks/LuaPlugin.java
+++ src/io/github/zacklukem/luablocks/LuaPlugin.java
@@ -1,6 +1,8 @@
 package io.github.zacklukem.luablocks;
 
+import org.luaj.vm2.Lua;
 import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.lib.jse.CoerceJavaToLua;
 import sun.plugin2.main.server.Plugin;
 
 import java.io.File;
@@ -12,7 +14,6 @@ public class LuaPlugin {
 
     private LuaValue pluginNamespace;
 
-
     public LuaPlugin(File file) {
         this.file = file;
         this.name = file.getName().replaceAll("\\.lua", "");
@@ -21,9 +22,9 @@ public class LuaPlugin {
         //System.out.println(name);
     }
 
-    public File getFile() {
-        return file;
-    }
+//    public File getFile() {
+//        return file;
+//    }
 
     private File file;
 
